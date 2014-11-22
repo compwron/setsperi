48d2363334fc528afff1d6082b5137eb9df4affd
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-11-22 01:22:54 -0800

    refactoring set validity check

--- lib/setsperi/card_set.rb
+++ lib/setsperi/card_set.rb
@@ -7,10 +7,9 @@
     def _valid?(cards)
       return false unless cards.size == 3
       return false if cards[0] == cards[1] && cards[1] == cards[2] # hack instead of .inject(&:==)
-      return false if cards.map(&:number).uniq.size == 2
-      return false if cards.map(&:symbol).uniq.size == 2
-      return false if cards.map(&:shading).uniq.size == 2
-      return false if cards.map(&:color).uniq.size == 2
+      [:number, :symbol, :shading, :color].each {|attribute| 
+        return false if cards.map(&attribute).uniq.size == 2
+      }
       true
     end
 
